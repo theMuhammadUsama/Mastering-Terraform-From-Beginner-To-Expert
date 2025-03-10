dc5ba01e0d844a78a678a4fa9f43704a2eb4a9a8
Usama <usama345327@gmail.com>
2025-03-10 05:41:57 +0500

    Terraform Course Started

---
+++ 02-hcl/hcl.tf
@@ -0,0 +1,33 @@
+terraform {
+    required_providers{
+        aws {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+#Activetly managed by us, Managed by our Terraform Project
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = var.bucket_name
+}
+
+#Managed somewhere else, we just want to use it in our project
+data "aws_s3_bucket" "my_external_bucket" {
+    bucket = ""
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = "my_default_bucket_name"
+  description = "This variable is used to set the bucket name"
+}
+
+output "bucket_id" {
+    value = aws_s3_bucket.my_bucket.id
+}
+
+locals {
+  local_example = "This is a local varaible/value"
+}
+
